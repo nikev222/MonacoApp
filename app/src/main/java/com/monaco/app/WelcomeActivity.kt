14de81fc3380e403c80895a474bf3b2d777ca328
nikev222 <ncortestorres7@gmail.com>
2025-10-19 23:20:21 -0500

    Entrega actividad 2 - Monaco App

---
+++ app/src/main/java/com/monaco/app/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package com.monaco.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        findViewById<Button>(R.id.btnComenzar).setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
+            finish()
+        }
+    }
+}
